6c6ff8c624c365f92c4f68e68e00000f35e29ab6
JuanjoCieri <juin@hotmail.com.ar>
2023-06-24 20:21:58 -0300

    add all resources

---
+++ src/resources/ideas/ideas.ts
@@ -0,0 +1,329 @@
+import { Resources } from "../interfaces";
+
+import background from "../../assets/resourcesImage/Image4.jpeg"
+
+import Awwwards from "./images/Awwwards.jpg";
+import Behance from "./images/Behance.jpg";
+import BestWebsiteGallery from "./images/BestWebsiteGallery.svg";
+import CalltoInspiration from "./images/CalltoInspiration.png";
+import CuratedDesign from "./images/CuratedDesign.svg";
+import DarkModeDesign from "./images/DarkModeDesign.svg";
+import DesignVault from "./images/DesignVault.svg";
+import Doingcoolstuff from "./images/Doingcoolstuff.svg";
+import Dribbble from "./images/Dribbble.jpg";
+import EmptyStates from "./images/EmptyStates.svg";
+import Figr from "./images/Figr.svg";
+import Godly from "./images/Godly.svg";
+import Httpster from "./images/Httpster.png";
+import HudsGuis from "./images/HudsGuis.svg";
+import Landbook from "./images/Landbook.svg";
+import Landdding from "./images/Landdding.svg";
+import Landings from "./images/Landings.svg";
+import LapaNinja from "./images/LapaNinja.jpg";
+import LaudableApps from "./images/LaudableApps.svg";
+import MinimalGallery from "./images/MinimalGallery.svg";
+import OgimageGallery from "./images/Ogimage.Gallery.jpg";
+import OnboardingLove from "./images/OnboardingLove.png";
+import OnePageLove from "./images/OnePageLove.png";
+import Pixels from "./images/Pixels.svg";
+import SaasExamples from "./images/SaasExamples.png";
+import SaasLandingPage from "./images/SaasLandingPage.jpg";
+import SaasPages from "./images/SaasPages.png";
+import Saaspo from "./images/Saaspo.svg";
+import Screenlane from "./images/Screenlane.svg";
+import Scrnshts from "./images/Scrnshts.jpg";
+import Siteinspire from "./images/Siteinspire.png";
+import TheFwa from "./images/TheFwa.svg";
+import UIGarage from "./images/UIGarage.jpg";
+import UpshiftSupply from "./images/UpshiftSupply.svg";
+import UXUIMonster from "./images/UXUIMonster.svg";
+import Webframe from "./images/Webframe.png";
+import a99designs from "./images/a99designs.png";
+import Unbounce from "./images/Unbounce.svg";
+import RMLPSenja from "./images/RMLP&Senja.png";
+import Hubspot from "./images/Hubspot.svg";
+
+
+
+const ideas: Resources[] = [
+    {
+        resourceName: "Ideas",
+        resourceImage: background
+    },
+    {
+        resourceCategories: [
+            {
+                categoryName: "Guías para la Inspiración",
+                categoryResources: [
+                    {
+                        resourceName: "99designs",
+                        resourceDesc: "Cómo diseñar una landing page: la guía definitiva",
+                        resourceImage: a99designs,
+                        resourceBackLink: "https://99designs.com/blog/web-digital/landing-page-design-how-to/"
+                    },
+                    {
+                        resourceName: "Unbounce",
+                        resourceDesc: "Prácticas recomendadas para la página de destino",
+                        resourceImage: Unbounce,
+                        resourceBackLink: "https://unbounce.com/landing-page-articles/landing-page-best-practices/"
+                    },
+                    {
+                        resourceName: "RMLP & Senja",
+                        resourceDesc: "Explicación de las páginas de destino",
+                        resourceImage: RMLPSenja,
+                        resourceBackLink: "https://landingpagesexplained.com/"
+                    },
+                    {
+                        resourceName: "Hubspot",
+                        resourceDesc: "La guía definitiva para las páginas de destino",
+                        resourceImage: Hubspot,
+                        resourceBackLink: "https://blog.hubspot.com/marketing/landing-page-best-practices/"
+                    },
+                ]
+            },
+            {
+                categoryName: "Ideas para Landing Pages",
+                categoryResources: [
+                    {
+                        resourceName: "Landbook",
+                        resourceDesc: "Sitios web seleccionados para inspiración creativa.",
+                        resourceImage: Landbook,
+                        resourceBackLink: "https://land-book.com/"
+                    },
+                    {
+                        resourceName: "Landdding",
+                        resourceDesc: "Diseños web diarios que muestran un gran diseño de la comunidad de diseñadores.",
+                        resourceImage: Landdding,
+                        resourceBackLink: "https://landdding.com/"
+                    },
+                    {
+                        resourceName: "Landings",
+                        resourceDesc: "Descubre cientos de páginas de destino para inspiración en diseño.",
+                        resourceImage: Landings,
+                        resourceBackLink: "https://landings.dev/"
+                    },
+                    {
+                        resourceName: "Lapa Ninja",
+                        resourceDesc: "Galería de más de 2000 ejemplos principales de páginas de destino en Lapa Ninja.",
+                        resourceImage: LapaNinja,
+                        resourceBackLink: "https://landings.dev/"
+                    },
+                    {
+                        resourceName: "One Page Love",
+                        resourceDesc: "Sitios web de una sola página, plantillas y recursos.",
+                        resourceImage: OnePageLove,
+                        resourceBackLink: "https://onepagelove.com/"
+                    },
+                    {
+                        resourceName: "Upshift Supply",
+                        resourceDesc: "Una colección inspiradora de las mejores páginas de destino.",
+                        resourceImage: UpshiftSupply,
+                        resourceBackLink: "https://www.upshift.supply/"
+                    },
+                    {
+                        resourceName: "UXUI Monster",
+                        resourceDesc: "Inspiración seleccionada de diseño para tu proyecto web.",
+                        resourceImage: UXUIMonster,
+                        resourceBackLink: "https://uxui.monster/"
+                    },
+                    {
+                        resourceName: "Webframe",
+                        resourceDesc: "Descubre e inspirate con hermosos diseños de aplicaciones web.",
+                        resourceImage: Webframe,
+                        resourceBackLink: "https://webframe.xyz/"
+                    },
+                ]
+            },
+            {
+                categoryName: "Inspiración UI",
+                categoryResources: [
+                    {
+                        resourceName: "Behance",
+                        resourceDesc: "Behance es donde los creativos descubren y muestran su trabajo.",
+                        resourceImage: Behance,
+                        resourceBackLink: "https://www.behance.net/"
+                    },
+                    {
+                        resourceName: "Call to Inspiration",
+                        resourceDesc: "Gorro morado digital para diseñadores de UX y desarrolladores.",
+                        resourceImage: CalltoInspiration,
+                        resourceBackLink: "https://calltoinspiration.com/"
+                    },
+                    {
+                        resourceName: "Design Vault",
+                        resourceDesc: "Patrones de UI e inspiración de diseño de productos reales.",
+                        resourceImage: DesignVault,
+                        resourceBackLink: "https://designvault.io/"
+                    },
+                    {
+                        resourceName: "Dribbble",
+                        resourceDesc: "Comparte trabajos de diseño, procesos y proyectos con la comunidad.",
+                        resourceImage: Dribbble,
+                        resourceBackLink: "https://dribbble.com/"
+                    },
+                    {
+                        resourceName: "Empty States",
+                        resourceDesc: "Una colección curada de diseños de interfaz de usuario sin datos.",
+                        resourceImage: EmptyStates,
+                        resourceBackLink: "https://emptystat.es/"
+                    },
+                    {
+                        resourceName: "Huds + Guis",
+                        resourceDesc: "Interfaces de usuario de películas y juegos.",
+                        resourceImage: HudsGuis,
+                        resourceBackLink: "https://www.hudsandguis.com/"
+                    },
+                    {
+                        resourceName: "Screenlane",
+                        resourceDesc: "La última inspiración en diseño web y móvil (anteriormente UI Movement).",
+                        resourceImage: Screenlane,
+                        resourceBackLink: "https://screenlane.com/"
+                    },
+                    {
+                        resourceName: "UI Garage",
+                        resourceDesc: "Inspiración y patrones de IU seleccionados diariamente para todas las plataformas.",
+                        resourceImage: UIGarage,
+                        resourceBackLink: "https://uigarage.net/"
+                    },
+                ]
+            },
+            {
+                categoryName: "Inspiración para Diseño Web",
+                categoryResources: [
+                    {
+                        resourceName: "Awwwards",
+                        resourceDesc: "Explora una gran cantidad de sitios web innovadores, creativos y visualmente atractivos.",
+                        resourceImage: Awwwards,
+                        resourceBackLink: "https://www.awwwards.com/"
+                    },
+                    {
+                        resourceName: "Best Website Gallery",
+                        resourceDesc: "Los sitios web más hermosos seleccionados especialmente para ti.",
+                        resourceImage: BestWebsiteGallery,
+                        resourceBackLink: "https://bestwebsite.gallery/"
+                    },
+                    {
+                        resourceName: "Curated Design",
+                        resourceDesc: "Catálogo de inspiración de diseño web fuera de este mundo para creadores.",
+                        resourceImage: CuratedDesign,
+                        resourceBackLink: "https://www.curated.design/"
+                    },
+                    {
+                        resourceName: "Dark Mode Design",
+                        resourceDesc: "Vitrina de sitios web bellamente diseñados en modo oscuro.",
+                        resourceImage: DarkModeDesign,
+                        resourceBackLink: "https://www.darkmodedesign.com/"
+                    },
+                    {
+                        resourceName: "Doingcoolstuff",
+                        resourceDesc: "Un directorio de estudios de diseño de marca y productos que hacen cosas geniales.",
+                        resourceImage: Doingcoolstuff,
+                        resourceBackLink: "https://www.doingcoolstuff.xyz/"
+                    },
+                    {
+                        resourceName: "Godly",
+                        resourceDesc: "La inspiración de diseño web más divina.",
+                        resourceImage: Godly,
+                        resourceBackLink: "https://godly.website/"
+                    },
+                    {
+                        resourceName: "Httpster",
+                        resourceDesc: "Muestra global de sitios web inspiradores creados por personas de todo el mundo.",
+                        resourceImage: Httpster,
+                        resourceBackLink: "https://httpster.net/"
+                    },
+                    {
+                        resourceName: "Minimal Gallery",
+                        resourceDesc: "Inspiración seleccionada para diseños web hermosos y funcionales.",
+                        resourceImage: MinimalGallery,
+                        resourceBackLink: "https://minimal.gallery/"
+                    },
+                    {
+                        resourceName: "Ogimage.Gallery",
+                        resourceDesc: "Galería con las mejores y más cuidadosamente recopiladas imágenes OG.",
+                        resourceImage: OgimageGallery,
+                        resourceBackLink: "https://www.ogimage.gallery/"
+                    },
+                    {
+                        resourceName: "Pixels",
+                        resourceDesc: "Inspiración de diseños web y escritura persuasiva.",
+                        resourceImage: Pixels,
+                        resourceBackLink: "https://klart.io/pixels"
+                    },
+                    {
+                        resourceName: "Siteinspire",
+                        resourceDesc: "Vitrina del mejor diseño web e interactivo.",
+                        resourceImage: Siteinspire,
+                        resourceBackLink: "https://www.siteinspire.com/"
+                    },
+                    {
+                        resourceName: "The Fwa",
+                        resourceDesc: "Innovación de vanguardia en diseño y desarrollo web.",
+                        resourceImage: TheFwa,
+                        resourceBackLink: "https://thefwa.com/"
+                    },
+                ]
+            },
+            {
+                categoryName: "Inspiración SaaS",
+                categoryResources: [
+                    {
+                        resourceName: "Figr",
+                        resourceDesc: "Diseño de UI editable que cumple con la inspiración del diseño web y de productos del mundo real.",
+                        resourceImage: Figr,
+                        resourceBackLink: "https://figr.design/"
+                    },
+                    {
+                        resourceName: "Onboarding Love",
+                        resourceDesc: "Más de 100 flujos de incorporación de usuarios de principales SaaS como Airbnb, Wix y Stripe.",
+                        resourceImage: OnboardingLove,
+                        resourceBackLink: "https://www.marbleflows.com/onboarding"
+                    },
+                    {
+                        resourceName: "Saas Examples",
+                        resourceDesc: "Galería de inspiración para tu sitio web, producto y correos electrónicos de SaaS.",
+                        resourceImage: SaasExamples,
+                        resourceBackLink: "https://divbyzero.com/tools/saas/"
+                    },
+                    {
+                        resourceName: "Saas Landing Page",
+                        resourceDesc: "Mejores páginas de destino de las principales empresas de SaaS para inspiración.",
+                        resourceImage: SaasLandingPage,
+                        resourceBackLink: "https://saaslandingpage.com/"
+                    },
+                    {
+                        resourceName: "Saas Pages",
+                        resourceDesc: "Mejores páginas de destino enfocadas en redacción y diseño.",
+                        resourceImage: SaasPages,
+                        resourceBackLink: "https://saaspages.xyz/"
+                    },
+                    {
+                        resourceName: "Saaspo",
+                        resourceDesc: "Una colección seleccionada de los mejores sitios web de SaaS en la web.",
+                        resourceImage: Saaspo,
+                        resourceBackLink: "https://www.saaspo.com/"
+                    },
+                ]
+            },
+            {
+                categoryName: "Inspiración para App's Moviles",
+                categoryResources: [
+                    {
+                        resourceName: "Laudable Apps",
+                        resourceDesc: "Vitrina independiente de hermosas aplicaciones de iOS.",
+                        resourceImage: LaudableApps,
+                        resourceBackLink: "https://laudableapps.com/"
+                    },
+                    {
+                        resourceName: "Scrnshts",
+                        resourceDesc: "Una selección de capturas de pantalla de diseño de la tienda de aplicaciones.",
+                        resourceImage: Scrnshts,
+                        resourceBackLink: "https://scrnshts.club/"
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+export default ideas;
